f9b2fafde00154ce2f603ef2afdb27e390d74328
songdongdong <songdongdong@weidian.com>
2021-02-18 10:18:04 +0800

    换钱的方法数

--- src/main/java/dynamic_programming/ExchangeMoneyFuc.java
+++ src/main/java/dynamic_programming/ExchangeMoneyFuc.java
@@ -28,19 +28,22 @@ package dynamic_programming;
  */
 public class ExchangeMoneyFuc {
     public static void main(String[] args) {
+        //暴力破解方式
         System.out.println(coins1(new int[]{5, 10, 25, 1}, 15));//6
         System.out.println(coins1(new int[]{3, 5}, 2));//0
-
+        //记忆搜索方式
+        System.out.println(coins2(new int[]{5, 10, 25, 1}, 15));//6
+        System.out.println(coins2(new int[]{3, 5}, 2));//0
         //动态规划方式计算，非最优
-        System.out.println(coins2(new int[]{5,10,25,1},15));//6
-        System.out.println(coins2(new int[]{3,5},2));//0
+        System.out.println(coins3(new int[]{5, 10, 25, 1}, 15));//6
+        System.out.println(coins3(new int[]{3, 5}, 2));//0
 
     }
 
 
     //换钱的方法数
     public static int coins1(int[] arr, int aim) {
-        //方法一：暴力递归，时间复杂度最差为O（aim^N）
+        //方法一：暴力递归，时间复杂度最差为O（aim^N） ，N表示数组的长度
         if (arr == null || arr.length == 0 || aim < 0) {
             return 0;
         }
@@ -64,8 +67,49 @@ public class ExchangeMoneyFuc {
         return res;
     }
 
-    //方法二：动态规划，非最优，时间复杂度为O（N×aim）
+
+    //基于暴⼒力递归的初步优化的⽅方法，也就是记忆搜索的⽅方法,暴⼒力递归之所以暴⼒力是因为存在⼤大量的重复计算
+    //⽐比如上⾯面的例⼦子，当已经使⽤用0张5元+1张10元的情况下， 后续应该求[25,1]组成剩下的990的⽅方法总数。当已经使⽤用2张5元+0张10元的情况下，后续还是求[25,1]组成剩下的990的⽅方法总数。
+    // 两个情况下都需要求process1(arr,2,990)。类似这 样的重复计算在暴⼒力递归的过程中⼤大量发⽣生，所以暴⼒力递归⽅方法的时间复杂度⾮非常⾼高并且 和arr中钱的⾯面值有关，最差情况下为O(aim^N)。
+    // 记忆化搜索的优化⽅方式。process1(arr,index,aim)中arr是始终不变的，变化的只有index和 aim，所以可以⽤用p(index,aim)表⽰示⼀一个递归过程。
+    // 重复计算之所以⼤大量发⽣生，是因为每⼀一 个递归过程的结果都没记下来，所以下次还要重复的去求。所以可以事先准备好⼀一个map， 每计算完⼀一个递归过程，都将结果记录到map中。
+    // 当下次进⾏行同样的递归过程之前，先在 map中查询是否这个递归过程已经计算过，如果已经计算过把值拿出来直接⽤用就可以了，如 果没计算过再进⼊入递归过程。具体请参看如下代码中的coins2⽅方法，它和coins1⽅方法的区别 就是准备好全局变量map，记录已经计算过的递归过程的结果，防⽌止下次重复计算。
+    // 因为本 题的递归过程可由两个变量表⽰示，所以map是⼀一张⼆二维表。map[i][j]表⽰示递归过程p(i,j)的 返回值。
+    // 另外有⼀一些特别值，map[i][j]==0表⽰示递归过程p(i,j)从来没有计算过。map[i] [j]==-1表⽰示递归过程p(i,j)计算过但返回值是0。
+    // 如果map[i][j]的值既不等于0也不等于-1， 记为a，则表⽰示递归过程p(i,j)的返回值为a。
     public static int coins2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+
+        }
+        int[][] map = new int[arr.length + 1][aim + 1];
+
+        return process2(arr, 0, aim, map);
+    }
+
+
+    public static int process2(int[] arr, int index, int aim, int[][] map) {
+        int res = 0;
+        if (index == arr.length) {
+            res = aim == 0 ? 1 : 0;
+        } else {
+            int mapValue = 0;
+            for (int i = 0; arr[index] * i <= aim; i++) {
+                mapValue = map[index + 1][aim - arr[index] * i];
+                if (mapValue != 0) {
+                    res += mapValue == -1 ? 0 : mapValue;
+                } else {
+                    res += process2(arr, index + 1, aim - arr[index] * i, map);
+                }
+            }
+        }
+        map[index][aim] = res == 0 ? -1 : res;
+        return res;
+    }
+
+
+    //方法二：动态规划，非最优，时间复杂度为O（N×aim） ，N表示数组的长度
+    public static int coins3(int[] arr, int aim) {
         if (arr == null || arr.length == 0 || aim < 0) {
             return 0;
         }
