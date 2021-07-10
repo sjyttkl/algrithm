62752957d9ca5477eafaaf1374b0c2fd29aa072f
songdongdong <songdongdong@weidian.com>
2021-07-10 15:13:27 +0800

    lcs，最长递增子序列

--- src/main/java/dynamic_programming/LCS/LongestCommonSequence.java
+++ src/main/java/dynamic_programming/LCS/LongestCommonSequence.java
@@ -1,5 +1,7 @@
 package dynamic_programming.LCS;
 
+import java.util.Arrays;
+
 /**
  * Create with: dynamic_programming
  * author: songdongdong
@@ -19,11 +21,13 @@ package dynamic_programming.LCS;
  */
 public class LongestCommonSequence {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{2, 1, 5, 3, 6, 4, 8, 9, 7};
+        lis1(arr);
+//        System.out.println(Arrays.toString(getdp1(arr)));
     }
 
     //时间复杂度为）O(N^2)
-    public int[] getdp1(int[] arr) {
+    public static int[] getdp1(int[] arr) {
         int[] dp = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             dp[i] = 1;//以当前位置结尾的字串长度默认 为1
@@ -34,7 +38,37 @@ public class LongestCommonSequence {
                 }
             }
         }
+        System.out.println("getdp1: " + Arrays.toString(dp));
         return dp;
     }
 
+    public static int[] generateLIS(int[] arr, int[] dp) {
+        int len = 0;
+        int index = 0;
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] > len) {
+                len = dp[i];
+                index = i;
+            }
+        }
+        int[] lis = new int[len];
+        lis[--len] = arr[index];
+        for (int i = index; i >= 0; i--) {
+            if (arr[i] < arr[index] && dp[i] == dp[index] - 1) {
+                lis[--len] = arr[i];
+                index = i;
+            }
+        }
+        System.out.println("generateLIS: " + Arrays.toString(lis));
+
+        return lis;
+    }
+
+    public static int[] lis1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        int[] dp = getdp1(arr);
+        return generateLIS(arr, dp);
+    }
 }
