87390a7aeea93ff23c81670718ca63f7c60389f0
songdongdong <songdongdong@weidian.com>
2021-07-11 14:34:02 +0800

    lcs，最长公共子序列

---
+++ src/main/java/dynamic_programming/LCS/longestCommonSubstring.java
@@ -0,0 +1,19 @@
+package dynamic_programming.LCS;
+
+/**
+ * Create with: dynamic_programming.LCS
+ * author: songdongdong
+ * E-mail: songdongdong@weidian.com
+ * date: 2021/7/11 12:09
+ * version: 1.0
+ * description: 最长公共子串问题
+ *
+ *
+ *给定两个字串 str1 和str2， 返回两个字符串的最长公共子串
+ *
+ * 举例： str1="1AB2345CD" , str2="12345EF", 返回："2345"
+ *
+ * 要求：如果str1 长度为M，str2长度为 N，实现时间复杂度O（M*N),空间复杂度为O（1）的方法
+ */
+public class longestCommonSubstring {
+}
