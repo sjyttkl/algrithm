33fa8ef7eceaf699bd79b32b6bc48fddd8375de5
songdongdong <songdongdong@weidian.com>
2021-07-11 14:44:17 +0800

    lcs，最长公共子序列

---
+++ src/main/java/dynamic_programming/LCS/longestCommonSequence.java
@@ -0,0 +1,21 @@
+package dynamic_programming.LCS;
+
+/**
+ * Create with: dynamic_programming.LCS
+ * author: songdongdong
+ * E-mail: songdongdong@weidian.com
+ * date: 2021/7/11 12:06
+ * version: 1.0
+ * description:最长公共子序列（longestOrdersequence）
+ * 给定两个字符串str1和str2，返回两个字符串最长的公共子序列
+ *
+ * 例如：
+ * str1="1A2C3dD4B56" ， str2="B1D23CA45B6A"
+ * "123456"或者"12C4B6" 都是公共子序列，返回哪一个都行
+ *
+ */
+public class longestCommonSequence {
+    public static void main(String[] args) {
+
+    }
+}
