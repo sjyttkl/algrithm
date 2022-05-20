835f8e97dfcca03e477573d1eb3caaacc75229ad
songdongdong <songdongdong@weidian.com>
2022-05-20 14:12:28 +0800

    完善代码

--- src/main/java/tree_base/ContainsT2.java
+++ src/main/java/tree_base/ContainsT2.java
@@ -6,31 +6,30 @@ package tree_base;
  * E-mail: 695492835@qq.com
  * date: 2020/2/15 0:31
  * version: 1.0
- * description:
+ * description:   给定彼此独立的两颗树头结点分别是t1和t2，判断t1树是否包含t2树全部的拓扑结构
  * 先序遍历树1，判断树1以每个节点为根的子树是否包含树2的拓扑结构。
  * 时间复杂度：O(M*N)
  * 注意区分判断总体包含关系、和判断子树是否包含树2的函数
  */
 public class ContainsT2 {
     public static void main(String args[]) {
-        //test
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        n1.left = n2;
-        n1.right = n3;
-        n3.left = n4;
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.left.left = new TreeNode(4);
+        node.left.left.left = new TreeNode(8);
+        node.left.left.right = new TreeNode(9);
+        node.left.right = new TreeNode(5);
+        node.left.right.left = new TreeNode(10);
+        node.right = new TreeNode(3);
+        node.right.left = new TreeNode(6);
+        node.right.right = new TreeNode(7);
 
-        TreeNode n5 = new TreeNode(3);
-        TreeNode n6 = new TreeNode(4);
-        n5.left = n6;
-
-        if (contains(n1, n5)) {
-            System.out.print("contains");
-        } else {
-            System.out.print("not contains");
-        }
+        TreeNode node1 = new TreeNode(2);
+        node1.left = new TreeNode(4);
+        node1.left.left = new TreeNode(8);
+        node1.right = new TreeNode(5);
+        boolean contains = contains(node, node1);
+        System.out.println(contains);
     }
 
     public static boolean contains(TreeNode t1, TreeNode t2) {
